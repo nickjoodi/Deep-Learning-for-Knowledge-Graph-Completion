872e9c3590afb4d23bdeabe15997bc2e48819d5b
Nicholas Joodi <joodi.5@osu.edu>
2017-11-30 08:04:35 -0800

    minor clean up

--- ntn/ntn.py
+++ ntn/ntn.py
@@ -377,18 +377,6 @@ with g.as_default():
         word_vectors = word_vectors[:500,:]
         words = words[:500]
 
-        def plot_with_labels(low_dim_embs, labels, filename='img/tsne_words_ntn.png'):
-            plt.figure(figsize=(18, 18)) 
-            for i, label in enumerate(labels):
-                x, y = low_dim_embs[i, :]
-                plt.scatter(x, y)
-                plt.annotate(label,
-                         xy=(x, y),
-                         xytext=(5, 2),
-                         textcoords='offset points',
-                         ha='right',
-                         va='bottom')
-            plt.savefig(filename)
 
         embedding = np.array(word_vectors)
         limit=500
