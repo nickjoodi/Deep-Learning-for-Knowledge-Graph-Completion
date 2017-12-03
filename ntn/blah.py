59049e2d5def0ca51fc7669b76e9d7559b64f2b0
Nicholas Joodi <joodi.5@osu.edu>
2017-12-03 00:10:09 -0800

    removed useless files

--- ntn/blah.py
+++
@@ -1,5 +0,0 @@
-import unicodecsv as csv
-with open('tSNE.csv', 'w') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',',
-                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    filewriter.writerow([1, 2,'oiy'])
